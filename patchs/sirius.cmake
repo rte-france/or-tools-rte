b3e8d82a9ff5667529a847886ea43f80158fd8fb
Jason Marechal <jason.marechal+github@gmail.com>
2024-08-06 16:02:22 +0200

    f

--- patchs/sirius.cmake
+++ patchs/sirius.cmake
@@ -6,11 +6,6 @@ FetchContent_Declare(sirius_solver
         GIT_REPOSITORY https://github.com/rte-france/sirius-solver
         GIT_TAG antares-integration-v1.5
         SOURCE_SUBDIR src
-        FETCHCONTENT_QUIET OFF
-        LOG_DOWNLOAD ON
-        LOG_PATCH ON
-        LOG_CONFIGURE ON
-        LOB_BUILD ON
         OVERRIDE_FIND_PACKAGE ON
 )
 FetchContent_MakeAvailable(sirius_solver)
